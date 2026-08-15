a51d8d07cd3d0c519a0dc7dc3f609c4d7acf4ac2
omecity <omeiza.olumoye@gmail.com>
2026-08-14 20:18:32 -0400

    Added the assets file that contains the ASSETS dictionary variable

---
+++ src/config/assets.py
@@ -0,0 +1,21 @@
+"""
+Configuration for the financial assets symbols and their corresponding market sectors
+"""
+
+
+ASSET = {
+    "BTC-USD": "Crypto",
+    "ETH-USD": "Crypto",
+    "SOL-USD": "Crypto",
+
+    "AAPL": "Tech",
+    "MSFT": "Tech",
+
+    "JPM": "Finance",
+    "BAC": "Finance",
+
+    "XOM": "Energy",
+    "CVX": "Energy",
+
+    "WMT": "Retail",
+}
